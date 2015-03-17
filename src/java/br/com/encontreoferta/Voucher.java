65ac854cc727c807c2bb6770ca972cb8e9d75fad
Leonardo de Souza <Leonardo.lpds@gmail.com>
2015-03-16 21:54:39 -0300

    Remove idCliente

--- src/java/br/com/encontreoferta/Voucher.java
+++ src/java/br/com/encontreoferta/Voucher.java
@@ -3,15 +3,13 @@ package br.com.encontreoferta;
 public class Voucher {
     int idVoucher;
     int idPromocao;
-    int idCliente;
 
     public Voucher() {
     }
 
-    public Voucher(int idVoucher, int idPromocao, int idCliente) {
+    public Voucher(int idVoucher, int idPromocao) {
         this.idVoucher = idVoucher;
         this.idPromocao = idPromocao;
-        this.idCliente = idCliente;
     }
     
     public int getIdVoucher() {
@@ -22,19 +20,11 @@ public class Voucher {
         return idPromocao;
     }
 
-    public int getIdCliente() {
-        return idCliente;
-    }
-
     public void setIdVoucher(int idVoucher) {
         this.idVoucher = idVoucher;
     }
 
     public void setIdPromocao(int idPromocao) {
         this.idPromocao = idPromocao;
-    }
-
-    public void setIdCliente(int idCliente) {
-        this.idCliente = idCliente;
     }
 }
