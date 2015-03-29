0013fe341e11dcef45f2f4d730d4dc664cdbd154
Leonardo de Souza <leonardo.lpds@gmail.com>
2015-03-29 16:38:42 -0300

    Alter tables.sql

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -1,4 +1,4 @@
-Create table vendedor(
+Create table app.vendedor(
     cnpj varchar(18) not null
         constraint pk_vendedor primary key,
     nomeFantasia varchar(50) not null,
@@ -10,20 +10,20 @@ Create table vendedor(
     senha varchar(50) not null
 );
 
-Create table categoria(
+Create table app.categoria(
     idCategoria int not null generated always as identity
         constraint pk_categoria primary key,
     nome varchar(50) not null,
     descricao varchar(100) not null
 );
 
-Create table promocao(
+Create table app.promocao(
     idPromocao int not null generated always as identity
         constraint pk_promocao primary key,
     cnpj varchar(18) not null,
-        constraint fk_promocao_vendedor foreign key (cnpj) references vendedor,
+        constraint fk_promocao_vendedor foreign key (cnpj) references app.vendedor,
     idCategoria int not null,
-        constraint fk_promocao_categoria foreign key (idCategoria) references categoria,
+        constraint fk_promocao_categoria foreign key (idCategoria) references app.categoria,
     titulo varchar(50) not null,
     descricao varchar(200) not null,
     valor decimal(7,2) not null,
@@ -32,9 +32,9 @@ Create table promocao(
     tempo date not null
 );
 
-Create table voucher(
+Create table app.voucher(
     idVoucher varchar(10) not null
         constraint pk_voucher primary key,
     idPromocao int not null,
-        constraint fk_voucher_promocao foreign key (idPromocao) references promocao
+        constraint fk_voucher_promocao foreign key (idPromocao) references app.promocao
 );
